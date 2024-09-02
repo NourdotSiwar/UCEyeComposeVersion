7e6970dfa3f2feb557448f27eebe882e47497d29
Nour Siwar <nour.siwar@outlook.com>
2024-09-01 20:21:05 -0700

    Added DAO operations/functions

--- app/src/main/java/com/example/uceyecomposeversion/model/room/MedicineDao.kt
+++ app/src/main/java/com/example/uceyecomposeversion/model/room/MedicineDao.kt
@@ -1,42 +1,45 @@
 package com.example.uceyecomposeversion.model.room
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MedicineDao {
 
+    // INSERT/REPLACE NEW/EXISTING ITEM
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMedicines(medicines: List<MedicineEntity>)
 
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    fun insert(medicineEntity: Entity): Long
+    // INSERT/REPLACE NEW/EXISTING SINGLE ITEM
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMedicine(medicines: List<MedicineEntity>)
 
-//    @Transaction
-//    fun insertMedicine(medicineEntities: List<Entity>) {
-//        medicineEntities.forEach {
-//            insert(it)
-//        }
-//    }
-
-    // Prescription may contain 1 or more medicines
+    // GET ALL MEDICINES
     @Query("SELECT * FROM medicinesDB")
-    fun getPrescription(): Flow<List<MedicineEntity>>
+    fun getAllMedicines(): Flow<List<MedicineEntity>>
 
-    @Query("SELECT * FROM medicinesDB WHERE medicineName = :medicineName LIMIT 1")
-    fun findByMedicineName(medicineName: String): MedicineEntity?
+    // GET SINGLE MEDICINE
+    @Query("SELECT * FROM medicinesDB WHERE medicineName = :medicineName")
+    fun getMedicineByName(medicineName: String): Flow<MedicineEntity?>
 
-//    //getMedicine() is used for the expiration date
-//    @Query("SELECT * FROM medicinesDB WHERE medicineName = :medicineName LIMIT 1")
-//    fun getMedicine(medicineName: String): Entity?
-//
-//    @Query("UPDATE medicinesDB SET expirationDate = :newExpirationDate WHERE medicineName = :medicineName")
-//    suspend fun editExpirationDate(medicineName: String, newExpirationDate: String)
+    // UPDATE SINGLE MEDICINE (May not be needed)
+    // Part of a new feature to update medicine fields
+    @Update
+    suspend fun updateMedicine(medicine: MedicineEntity)
 
-//    @Query("DELETE FROM medicinesDB WHERE medicineName IN (:medicines)")
-//    suspend fun deletePrescription(medicines: List<String>)
-//
-//    @Query("DELETE FROM medicinesDB WHERE medicineName = :medicineName")
-//    suspend fun deleteMedicine(medicineName: String)
+    // UPDATE SINGLE MEDICINE'S EXPIRATION DATE
+    // TODO: @Query("UPDATE medicinesDB SET expirationDate = :newExpirationDate WHERE medicineName = :medicineName")
+    // TODO:  suspend fun updateExpirationDate(medicineName: String, newExpirationDate: String)
 
-    // TODO: editMedicine() <- entirely near feature
+    // DELETE SINGLE ITEM
+    @Query("DELETE FROM medicinesDB WHERE medicineName = :medicineName")
+    suspend fun deleteMedicine(medicineName: String)
+
+    // DELETE ALL MEDICINES
+    @Query("DELETE FROM medicinesDB")
+    suspend fun deleteAllMedicines()
 }
